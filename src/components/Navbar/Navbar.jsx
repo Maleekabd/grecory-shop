f739aabe8dc8088dfbc71100d70346e36364e3fd
Maleekabd <malikabdurrasyid86@gmail.com>
2024-07-14 13:59:47 +0700

    fixed : add logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import Input from "../Elements/Input";
 import Data from "../Products/DummyData.json";
 import "./Navbar.css";
 import SubNavbar from "./SubNavbar";
+import Grocery from "../../../public/grocery.png"
 
 const size = 25;
 
@@ -15,7 +16,7 @@ export const Navbar = () => {
     <header className="input sticky top-0 z-10 pt-0 px-0 pb-2">
       <nav className="flex sm:flex-wrap md:flex-wrap lg:flex-nowrap  justify-evenly items-center p-5">
         <a href="/">
-          <img src="" alt="logo" />
+          <img src={Grocery} className="w-14" alt="logo" />
         </a>
         <Input />
         <ul className="flex list-none gap-5">
