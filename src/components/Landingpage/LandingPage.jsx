ff45c6cd5e12f830c5faaf0b00600efa37b15c4f
Maleekabd <malikabdurrasyid86@gmail.com>
2024-09-08 19:40:08 +0700

    updated

--- src/components/Landingpage/LandingPage.jsx
+++ src/components/Landingpage/LandingPage.jsx
@@ -52,33 +52,33 @@ export const LandingPage = () => {
             </Swiper>
           </figure>
           <Text />
-          <LandingPage2 />
+          {/* <LandingPage2 /> */}
         </div>
       </div>
     </section>
   );
 };
 
-const LandingPage2 = () => {
-  return (
-    <>
-      <figure className="m-5 pt-8 gap-2  grid grid-cols-2">
-        <img
-          className={`h-full w-full row-span-2`}
-          src="https://images.pexels.com/photos/7728347/pexels-photo-7728347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Organic Produce"
-        />
-        <img
-          className={`h-[15em] w-full`}
-          src="https://images.pexels.com/photos/7965886/pexels-photo-7965886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Butter and Margarine"
-        />
-        <img
-          className={`h-[15em] w-full`}
-          src="https://images.pexels.com/photos/96379/pexels-photo-96379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Seafood"
-        />
-      </figure>
-    </>
-  );
-};
+// const LandingPage2 = () => {
+//   return (
+//     <>
+//       <figure className="m-5 pt-8 gap-2  grid grid-cols-2">
+//         <img
+//           className={`h-full w-full row-span-2`}
+//           src="https://images.pexels.com/photos/7728347/pexels-photo-7728347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//           alt="Organic Produce"
+//         />
+//         <img
+//           className={`h-[15em] w-full`}
+//           src="https://images.pexels.com/photos/7965886/pexels-photo-7965886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//           alt="Butter and Margarine"
+//         />
+//         <img
+//           className={`h-[15em] w-full`}
+//           src="https://images.pexels.com/photos/96379/pexels-photo-96379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//           alt="Seafood"
+//         />
+//       </figure>
+//     </>
+//   );
+// };
